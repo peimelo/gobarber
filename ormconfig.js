73126bef4c1158a4bd14d33ae8b4654f2a40b3af
Paulo Melo <peimelo@gmail.com>
2020-04-25 17:35:24 -0300

    Heroku Build

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'docker',
-    database: 'postgres',
-    entities: ['/app/dist/models/*.js'],
-    migrations: ['/app/dist/database/migrations/*.js'],
-    cli: {
-      migrationsDir: '/app/dist/database/migrations',
-    },
-  };
-} else {
+if (process.env.NODE_ENV === 'development') {
   module.exports = {
     type: 'postgres',
     host: 'localhost',
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,19 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+if (process.env.NODE_ENV === 'production') {
+  createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'postgres',
+    entities: ['../models/*.js'],
+    migrations: ['./migrations/*.js'],
+    cli: {
+      migrationsDir: './migrations',
+    },
+  });
+} else {
+  createConnection();
+}
