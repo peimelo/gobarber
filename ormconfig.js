8ac300086ba8c5a3e9535b1a102400da243f5036
Paulo Melo <peimelo@gmail.com>
2020-04-25 19:55:57 -0300

    Heroku Deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,13 @@
-import { Appointment } from './src/models/Appointment.entity';
-
 const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'postgres',
-  entities: [Appointment],
+  host: process.env.TYPEORM_HOST || 'localhost',
+  port: process.env.TYPEORM_PORT || 5432,
+  username: process.env.TYPEORM_USERNAME || 'postgres',
+  password: process.env.TYPEORM_PASSWORD || 'docker',
+  database: process.env.TYPEORM_DATABASE || 'postgres',
+  entities: [rootDir + '/**/*.entity{.js,.ts}'],
   migrations: [rootDir + '/database/migrations/*{.js,.ts}'],
   cli: {
     migrationsDir: rootDir + '/database/migrations',
