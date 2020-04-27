498365b410f786126f954d60bbf6474c19da8a7b
Paulo Melo <peimelo@gmail.com>
2020-04-27 06:37:24 -0300

    Create User

---
+++ src/database/migrations/1587906952043-AlterProviderInAppointments.ts
@@ -0,0 +1,52 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AlterProviderInAppointments1587906952043
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'providerId',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        columnNames: ['providerId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('appointments');
+    const foreignKey =
+      table.foreignKeys.find(
+        fk => fk.columnNames.indexOf('providerId') !== -1,
+      ) || '';
+    await queryRunner.dropForeignKey('appointments', foreignKey);
+
+    await queryRunner.dropColumn('appointments', 'providerId');
+
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
+}
--- src/models/Appointment.entity.ts
+++ src/models/Appointment.entity.ts
@@ -2,9 +2,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import User from './User.entity';
 
 @Entity('appointments')
 class Appointment {
@@ -12,7 +15,11 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  providerId: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'providerId' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
